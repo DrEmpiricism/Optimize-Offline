ae57f15f9cb12411488323777e12e1c142a238a5
Ben <DrEmpiricism@users.noreply.github.com>
2018-07-10 18:24:15 -0400

    Update Start.cmd

--- Start.cmd
+++ Start.cmd
@@ -6,8 +6,9 @@ REM The variables carry over to the PowerShell script, as do the switches.
 
 REM Start PowerShell Variables
 SET "SourcePath=X:\PathToImage"
-SET "SourceBuild=16299"
-SET "DriverPath=X:\PathToDriverPackages"
+SET "SourceBuild=17134"
+SET "DriverPath=X:\PathTo\DriverPackages"
+SET "NetFx3Path=X:\PathTo\NetFx3Cab"
 REM End PowerShell Variables
 
 NET SESSION >NUL 2>&1
@@ -19,7 +20,7 @@ IF %ERRORLEVEL% NEQ 0 (
 ) ELSE (
     ECHO Running as Administrator.
     TIMEOUT /T 2 >NUL
-    PowerShell.exe -NoProfile -ExecutionPolicy Bypass -File .\Optimize-Offline.ps1 -ImagePath "%SourcePath%" -Build %SourceBuild% -SelectApps -SystemApps -SetRegistry -OptionalFeatures -OnDemandPackages
+    PowerShell.exe -NoProfile -ExecutionPolicy Bypass -File .\Optimize-Offline-Github.ps1 -ImagePath "%SourcePath%" -Build %SourceBuild% -MetroApps "Select" -SystemApps -Registry "Default" -Packages -Features -Drivers "%DriverPath%" -NetFx3 "%NetFx3Path%"
 )
 PAUSE
 EXIT
