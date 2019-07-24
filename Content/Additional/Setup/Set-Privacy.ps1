9f83c7ac14512a739378f117a5cc0496d3fb419b
Ben <DrEmpiricism@users.noreply.github.com>
2019-07-23 23:23:10 -0400

    Updated.

--- Content/Additional/Setup/Set-Privacy.ps1
+++ Content/Additional/Setup/Set-Privacy.ps1
@@ -151,18 +151,18 @@ Function Set-Privacy
     [CmdletBinding()]
     Param ()
 
-    $ErrorActionPreference = 'SilentlyContinue'; $WarningPreference = 'SilentlyContinue'; $ProgressPreference = 'SilentlyContinue'; Clear-Host
+    Clear-Host
     Write-Host "Setting Additional Privacy Restrictions..." -NoNewline -ForegroundColor Cyan
 
     # Disables all scheduled tasks listed in the TaskList that are found to be present on the system.
-    Get-ScheduledTask | Where-Object TaskName -In $TaskList | Where-Object State -NE Disabled | Disable-ScheduledTask
+    Get-ScheduledTask | Where-Object TaskName -In $TaskList | Where-Object State -NE Disabled | Disable-ScheduledTask -ErrorAction SilentlyContinue | Out-Null
 
     # Saves the original startup type of all services listed in the ServicesList that are found to be present on the system.
     $ServicesDefault = @{ }
     Get-Service | Where-Object Name -In $ServiceList | ForEach-Object { $ServicesDefault.Add($_.Name, $_.StartType) }
     $ServicesDefault | Out-File -FilePath .\ServicesDefault.log -Force
     # Stops and disables all services listed in the ServicesList that are found to be present on the system.
-    Get-Service | Where-Object Name -In $ServiceList | Where-Object { $_.StartType -ne 'Disabled' } | Set-Service -StartupType Disabled
+    Get-Service | Where-Object Name -In $ServiceList | Where-Object { $_.StartType -ne 'Disabled' } | Set-Service -StartupType Disabled -ErrorAction SilentlyContinue | Out-Null
 
     # Disables Autologgers
     Set-AutologgerConfig -Name AutoLogger-Diagtrack-Listener -Start 0
@@ -171,24 +171,23 @@ Function Set-Privacy
     Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\WMI\AutoLogger\SQMLogger" -Name "Start" -Value 0 -Type DWord
 
     # Modifies the firewall rules and adds additional outbound blocks for Windows Apps, CompatTelRunner, DiagTrack and SmartScreen.
-    Get-NetFirewallRule | Where-Object Group -Like "*@{*" | Remove-NetFirewallRule
-    Get-NetFirewallRule | Where-Object Group -EQ "DiagTrack" | Remove-NetFirewallRule
-    Get-NetFirewallRule | Where-Object DisplayGroup -EQ "Delivery Optimization" | Remove-NetFirewallRule
-    Get-NetFirewallRule | Where-Object DisplayGroup -Like "Windows Media Player Network Sharing Service*" | Remove-NetFirewallRule
-    New-NetFirewallRule -DisplayName "Block DiagTrack" -Action Block -Description "Prevent DiagTrack Outbound Traffic." -Direction Outbound -Name "DiagTrack" -Profile Any -Service DiagTrack
-    New-NetFirewallRule -DisplayName "Block Compatability Telemetry Runner" -Action Block -Description "Prevent CompatTelRunner Outbound Traffic." -Direction Outbound -Name "Compatability Telemetry Runner" -Profile Any -Program "%SystemDrive%\Windows\System32\CompatTelRunner.exe"
-    New-NetFirewallRule -DisplayName "Block SmartScreen" -Action Block -Description "Prevent SmartScreen Outbound Traffic." -Direction Outbound -Name "SmartScreen" -Profile Any -Program "%SystemDrive%\Windows\System32\smartscreen.exe"
-    New-NetFirewallRule -DisplayName "Block Windows Error Reporting" -Action Block -Description "Prevent Windows Error Reporting Outbound Traffic." -Direction Outbound -Name "Windows Error Reporting" -Profile Any -Program "%SystemDrive%\Windows\System32\svchost.exe" -Protocol TCP -RemotePort "80, 443" -Service WerSvc
+    Get-NetFirewallRule | Where-Object Group -Like "*@{*" | Remove-NetFirewallRule -ErrorAction SilentlyContinue | Out-Null
+    Get-NetFirewallRule | Where-Object Group -EQ "DiagTrack" | Remove-NetFirewallRule -ErrorAction SilentlyContinue | Out-Null
+    Get-NetFirewallRule | Where-Object DisplayGroup -EQ "Delivery Optimization" | Remove-NetFirewallRule -ErrorAction SilentlyContinue | Out-Null
+    Get-NetFirewallRule | Where-Object DisplayGroup -Like "Windows Media Player Network Sharing Service*" | Remove-NetFirewallRule -ErrorAction SilentlyContinue | Out-Null
+    New-NetFirewallRule -DisplayName "Block DiagTrack" -Action Block -Description "Prevent DiagTrack Outbound Traffic." -Direction Outbound -Name "DiagTrack" -Profile Any -Service DiagTrack -ErrorAction SilentlyContinue | Out-Null
+    New-NetFirewallRule -DisplayName "Block Compatability Telemetry Runner" -Action Block -Description "Prevent CompatTelRunner Outbound Traffic." -Direction Outbound -Name "Compatability Telemetry Runner" -Profile Any -Program "%SystemDrive%\Windows\System32\CompatTelRunner.exe" -ErrorAction SilentlyContinue | Out-Null
+    New-NetFirewallRule -DisplayName "Block SmartScreen" -Action Block -Description "Prevent SmartScreen Outbound Traffic." -Direction Outbound -Name "SmartScreen" -Profile Any -Program "%SystemDrive%\Windows\System32\smartscreen.exe" -ErrorAction SilentlyContinue | Out-Null
 
     # Disables Swapfile.sys which can improve SSD performance.
-    Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" -Name "SwapfileControl" -Value 0 -Type DWord
+    Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" -Name "SwapfileControl" -Value 0 -Type DWord | Out-Null
 
     # Removes leftover DiagTrack and Windows Defender logs.
-    Remove-Item -Path "$Env:SystemRoot\System32\LogFiles\WMI\AutoLogger-Diagtrack-Listener.etl" -Force
-    Remove-Item -Path "$Env:ProgramData\Microsoft\Diagnosis\ETLLogs\AutoLogger\AutoLogger-Diagtrack-Listener.etl" -Force
-    Remove-Item -Path "$Env:ProgramData\Microsoft\Diagnosis\ETLLogs\*" -Recurse -Force
-    Remove-Item -Path "$Env:ProgramData\Microsoft\Diagnosis\*.rbs" -Recurse -Force
-    Remove-Item -Path "$Env:ProgramData\Microsoft\Windows Defender\Scans\*" -Recurse -Force
+    Remove-Item -Path "$Env:SystemRoot\System32\LogFiles\WMI\AutoLogger-Diagtrack-Listener.etl" -Force -ErrorAction SilentlyContinue | Out-Null
+    Remove-Item -Path "$Env:ProgramData\Microsoft\Diagnosis\ETLLogs\AutoLogger\AutoLogger-Diagtrack-Listener.etl" -Force -ErrorAction SilentlyContinue | Out-Null
+    Remove-Item -Path "$Env:ProgramData\Microsoft\Diagnosis\ETLLogs\*" -Recurse -Force -ErrorAction SilentlyContinue | Out-Null
+    Remove-Item -Path "$Env:ProgramData\Microsoft\Diagnosis\*.rbs" -Recurse -Force -ErrorAction SilentlyContinue | Out-Null
+    Remove-Item -Path "$Env:ProgramData\Microsoft\Windows Defender\Scans\*" -Recurse -Force -ErrorAction SilentlyContinue | Out-Null
 
     # Clears the DNS Cache
     Clear-DnsClientCache
