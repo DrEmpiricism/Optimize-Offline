e94f80d56877e84f4d67a0ba3d957464ca8e62cf
Ben <DrEmpiricism@users.noreply.github.com>
2020-02-17 15:31:12 -0500

    Updated C# code.

--- Src/Private/Grant-Privilege.ps1
+++ Src/Private/Grant-Privilege.ps1
@@ -11,7 +11,6 @@ Function Grant-Privilege
 
     Begin
     {
-        Set-ErrorAction SilentlyContinue
         Add-Type @'
 using System;
 using System.ComponentModel;
@@ -19,28 +18,28 @@ using System.Runtime.InteropServices;
 
 public class AccessToken
 {
-    [DllImport ("advapi32.dll", SetLastError = true)] static extern bool LookupPrivilegeValue (String Host, String Name, ref long luid);
-    [DllImport ("advapi32.dll", ExactSpelling = true, SetLastError = true)] static extern bool AdjustTokenPrivileges (IntPtr Token, bool disall, ref TOKEN_PRIVILEGES newst, int len, IntPtr prev, IntPtr relen);
-    [DllImport ("advapi32.dll", ExactSpelling = true, SetLastError = true)] static extern bool OpenProcessToken (IntPtr CurrentProcess, int acc, ref IntPtr ProcessToken);
-    [DllImport ("kernel32.dll", SetLastError = true)] static extern bool CloseHandle (IntPtr Handle);
+    [DllImport ("advapi32.dll", SetLastError = true)] static extern bool LookupPrivilegeValue (String Host, String Name, ref long Luid);
+    [DllImport ("advapi32.dll", ExactSpelling = true, SetLastError = true)] static extern bool AdjustTokenPrivileges (IntPtr TokenHandle, bool RevokeAllPrivileges, ref TOKEN_PRIVILEGES NewTokenState, Int32 BufferLength, IntPtr PreviousTokenState, IntPtr ReturnLength);
+    [DllImport ("advapi32.dll", ExactSpelling = true, SetLastError = true)] static extern bool OpenProcessToken (IntPtr ProcessToken, Int32 DesiredAccess, ref IntPtr hObject);
+    [DllImport ("kernel32.dll", SetLastError = true)] static extern bool CloseHandle (IntPtr hObject);
 
-    [StructLayout (LayoutKind.Sequential, Pack = 1)] struct TOKEN_PRIVILEGES { public int Count; public long Luid; public int Attr; }
+    [StructLayout (LayoutKind.Sequential, Pack = 1)] struct TOKEN_PRIVILEGES { public Int32 PrivilegeCount; public long Luid; public Int32 Attributes; }
 
-    internal const int SE_PRIVILEGE_DISABLED = 0x00000000;
-    internal const int SE_PRIVILEGE_ENABLED = 0x00000002;
-    internal const int TOKEN_QUERY = 0x00000008;
-    internal const int TOKEN_ADJUST_PRIVILEGES = 0x00000020;
+    internal const Int32 SE_PRIVILEGE_DISABLED = 0x00000000;
+    internal const Int32 SE_PRIVILEGE_ENABLED = 0x00000002;
+    internal const Int32 TOKEN_QUERY = 0x00000008;
+    internal const Int32 TOKEN_ADJUST_PRIVILEGES = 0x00000020;
 
-    public static void AdjustPrivilege (IntPtr CurrentProcess, String Privilege, bool enable)
+    public static void AdjustPrivilege (IntPtr ProcessToken, String Privilege, bool Enable)
     {
-        var ProcessToken = IntPtr.Zero;
-        if (!OpenProcessToken (CurrentProcess, TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, ref ProcessToken)) { throw new Win32Exception (); }
+        var TokenHandle = IntPtr.Zero;
+        if (!OpenProcessToken (ProcessToken, TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, ref TokenHandle)) { throw new Win32Exception (); }
         try
         {
-            var Privileges = new TOKEN_PRIVILEGES { Count = 1, Luid = 0, Attr = enable ? SE_PRIVILEGE_ENABLED : SE_PRIVILEGE_DISABLED, };
-            if (!LookupPrivilegeValue (null, Privilege, ref Privileges.Luid) || !AdjustTokenPrivileges (ProcessToken, false, ref Privileges, 0, IntPtr.Zero, IntPtr.Zero)) { throw new Win32Exception (); }
+            var Privileges = new TOKEN_PRIVILEGES { PrivilegeCount = 1, Luid = 0, Attributes = Enable ? SE_PRIVILEGE_ENABLED : SE_PRIVILEGE_DISABLED, };
+            if (!LookupPrivilegeValue (null, Privilege, ref Privileges.Luid) || !AdjustTokenPrivileges (TokenHandle, false, ref Privileges, 0, IntPtr.Zero, IntPtr.Zero)) { throw new Win32Exception (); }
         }
-        finally { CloseHandle (ProcessToken); }
+        finally { CloseHandle (TokenHandle); }
     }
 }
 '@
@@ -48,11 +47,10 @@ public class AccessToken
     }
     Process
     {
-        $Privilege | ForEach-Object -Process { [AccessToken]::AdjustPrivilege($CurrentProcess.Handle, $PSItem, !$Disable) }
+        $Privilege | ForEach-Object -Process { [AccessToken]::AdjustPrivilege($CurrentProcess.Handle, $PSItem, !$Disable.IsPresent) }
     }
     End
     {
         $CurrentProcess.Close()
-        Set-ErrorAction -Restore
     }
 }
