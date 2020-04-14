6b7f0d42723de6886cbddf2587e85fa91c3e39c4
Ben <DrEmpiricism@users.noreply.github.com>
2020-04-14 15:40:29 -0400

    Removed the automatic disabling of hibernation.

--- Content/Additional/Setup/SetupComplete.cmd
+++ Content/Additional/Setup/SetupComplete.cmd
@@ -2,7 +2,6 @@ CD /D "%~dp0"
 
 REM Answer files should ALWAYS be removed from the 'Windows\Panther' and 'Windows\System32\Sysprep' directories immediately after they've been processed.
 
-POWERCFG -H OFF >NUL 2>&1
 PowerShell.exe -NoProfile -ExecutionPolicy Bypass -File "%WINDIR%\Setup\Scripts\Refresh-Explorer.ps1"
 DEL /F /Q "%WINDIR%\Setup\Scripts\Refresh-Explorer.ps1" >NUL 2>&1
 DEL /F /Q "%WINDIR%\Panther\unattend.xml" >NUL 2>&1
