30ac91138396fc126fc654f0fe90753725c2f3f7
Ben <DrEmpiricism@users.noreply.github.com>
2020-04-25 20:26:02 -0400

    Merged hive testing into the load and unload processes..

--- Src/Public/Get-OfflineHives.ps1
+++ Src/Public/Get-OfflineHives.ps1
@@ -6,57 +6,56 @@ Function Get-OfflineHives
         [Parameter(ParameterSetName = 'Load')]
         [Switch]$Load,
         [Parameter(ParameterSetName = 'Unload')]
-        [Switch]$Unload,
-        [Parameter(ParameterSetName = 'Test')]
-        [Switch]$Test
+        [Switch]$Unload
     )
 
     Begin
     {
+        $Hive = [Ordered]@{ SOFTWARE = (GetPath -Path $InstallMount -Child 'Windows\System32\Config\SOFTWARE'); SYSTEM = (GetPath -Path $InstallMount -Child 'Windows\System32\Config\SYSTEM'); DEFAULT = (GetPath -Path $InstallMount -Child 'Windows\System32\Config\DEFAULT'); NTUSER = (GetPath -Path $InstallMount -Child 'Users\Default\NTUSER.DAT') }
+        $HiveMountPoint = [Ordered]@{ SOFTWARE = 'WIM_HKLM_SOFTWARE'; SYSTEM = 'WIM_HKLM_SYSTEM'; DEFAULT = 'WIM_HKU_DEFAULT'; NTUSER = 'WIM_HKCU' }
         $HKLM = 0x80000002
-        $Hive = [Ordered]@{
-            SOFTWARE = (GetPath -Path $InstallMount -Child 'Windows\System32\Config\SOFTWARE')
-            SYSTEM   = (GetPath -Path $InstallMount -Child 'Windows\System32\Config\SYSTEM')
-            DEFAULT  = (GetPath -Path $InstallMount -Child 'Windows\System32\Config\DEFAULT')
-            NTUSER   = (GetPath -Path $InstallMount -Child 'Users\Default\NTUSER.DAT')
-        }
-        $HiveMountPoint = [Ordered]@{
-            SOFTWARE = 'WIM_HKLM_SOFTWARE'
-            SYSTEM   = 'WIM_HKLM_SYSTEM'
-            DEFAULT  = 'WIM_HKU_DEFAULT'
-            NTUSER   = 'WIM_HKCU'
-        }
+        'SeBackupPrivilege', 'SeRestorePrivilege' | Grant-Privilege
     }
     Process
     {
+        $HivesMounted = $HiveMountPoint.Values.ForEach{ If (Test-Path -LiteralPath $PSItem.Insert(0, 'HKLM:\')) { $true } }
         Switch ($PSCmdlet.ParameterSetName)
         {
             'Load'
             {
-                $RegLoad = Import-Win32API -Load
-                'SeBackupPrivilege', 'SeRestorePrivilege' | Grant-Privilege
-                [Void]$RegLoad::RegLoadKey($HKLM, $HiveMountPoint.SOFTWARE, $Hive.SOFTWARE)
-                [Void]$RegLoad::RegLoadKey($HKLM, $HiveMountPoint.SYSTEM, $Hive.SYSTEM)
-                [Void]$RegLoad::RegLoadKey($HKLM, $HiveMountPoint.DEFAULT, $Hive.DEFAULT)
-                [Void]$RegLoad::RegLoadKey($HKLM, $HiveMountPoint.NTUSER, $Hive.NTUSER)
-                'SeBackupPrivilege', 'SeRestorePrivilege' | Grant-Privilege -Disable
+                If (!$HivesMounted)
+                {
+                    $RegLoad = Import-Win32API -Load
+                    [Void]$RegLoad::RegLoadKey($HKLM, $HiveMountPoint.SOFTWARE, $Hive.SOFTWARE)
+                    [Void]$RegLoad::RegLoadKey($HKLM, $HiveMountPoint.SYSTEM, $Hive.SYSTEM)
+                    [Void]$RegLoad::RegLoadKey($HKLM, $HiveMountPoint.DEFAULT, $Hive.DEFAULT)
+                    [Void]$RegLoad::RegLoadKey($HKLM, $HiveMountPoint.NTUSER, $Hive.NTUSER)
+                }
                 Break
             }
             'Unload'
             {
-                $RegUnload = Import-Win32API -Unload
-                'SeBackupPrivilege', 'SeRestorePrivilege' | Grant-Privilege
-                [GC]::Collect()
-                [GC]::WaitForPendingFinalizers()
-                $HiveMountPoint.Values.ForEach{ [Void]$RegUnload::RegUnLoadKey($HKLM, $PSItem) }
-                'SeBackupPrivilege', 'SeRestorePrivilege' | Grant-Privilege -Disable
-                Break
-            }
-            'Test'
-            {
-                $HiveMountPoint.Values.ForEach{ If (Test-Path -Path $PSItem.Insert(0, 'HKLM:\')) { $true } }
+                If ($HivesMounted)
+                {
+                    $RegUnload = Import-Win32API -Unload
+                    [GC]::Collect()
+                    [GC]::WaitForPendingFinalizers()
+                    $Retries = 5
+                    While ($Retries -gt 0 -and $HivesMounted)
+                    {
+                        $HiveMountPoint.Values.ForEach{ [Void]$RegUnload::RegUnLoadKey($HKLM, $PSItem) }
+                        Start-Sleep 1
+                        $HivesMounted = $HiveMountPoint.Values.ForEach{ If (Test-Path -LiteralPath $PSItem.Insert(0, 'HKLM:\')) { $true } }
+                        If ($HivesMounted) { Start-Sleep 5 }
+                        $Retries--
+                    }
+                }
                 Break
             }
         }
+    }
+    End
+    {
+        'SeBackupPrivilege', 'SeRestorePrivilege' | Grant-Privilege -Disable
     }
 }
