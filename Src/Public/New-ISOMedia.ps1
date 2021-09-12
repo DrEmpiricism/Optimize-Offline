6e4874bef8aca04cd14effff1adc595979ad4f67
Ben <DrEmpiricism@users.noreply.github.com>
2021-09-12 11:36:37 -0400

    New-ISOMedia function now supports PShell 6+

--- Src/Public/New-ISOMedia.ps1
+++ Src/Public/New-ISOMedia.ps1
@@ -19,88 +19,64 @@ Function New-ISOMedia
         }
         If ($PSVersionTable.PSVersion.Major -gt 5 -and !(Test-Path -Path (GetPath -Path $ISOMedia.FullName -Child 'boot\etfsboot.com'))) { Log "Missing the required etfsboot.com bootfile for ISO creation." -Type Error; Start-Sleep 3; Break }
         If (!(Test-Path -Path (GetPath -Path $ISOMedia.FullName -Child "efi\Microsoft\boot\$($BootFile)"))) { Log ('Missing the required {0} bootfile for ISO creation.' -f $BootFile) -Type Error; Start-Sleep 3; Break }
-        If ($PSVersionTable.PSVersion.Major -lt 6)
+        If (!('ISOWriter' -as [Type]))
         {
-            $CompilerParams = New-Object -TypeName CodeDom.Compiler.CompilerParameters -Property @{ CompilerOptions = '/unsafe'; WarningLevel = 4; TreatWarningsAsErrors = $true } -ErrorAction:$ErrorActionPreference
-            If (!('ISOWriter' -as [Type]))
-            {
-                Add-Type @'
+            Add-Type -TypeDefinition @'
 using System;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.ComTypes;
 
 public class ISOWriter
 {
-    public unsafe static void Create(string Path, object Stream, int BlockSize, int TotalBlocks)
+    [DllImport ("shlwapi.dll", CharSet = CharSet.Unicode, ExactSpelling = true, PreserveSig = false, EntryPoint = "SHCreateStreamOnFileEx")]
+    internal static extern void SHCreateStreamOnFileEx (String FileName, UInt32 Mode, UInt32 Attributes, Boolean Create, IStream StreamNull, out IStream Stream);
+
+    public static void Create (String FilePath, ref Object ImageStream, Int32 BlockSize, Int32 TotalBlocks)
     {
-        int BytesRead = 0;
-        byte[] Buffer = new byte[BlockSize];
-        int* Pointer = &BytesRead;
-        var OpenFile = System.IO.File.OpenWrite(Path);
-        var IStream = Stream as System.Runtime.InteropServices.ComTypes.IStream;
-        if (OpenFile != null)
+        IStream ResultStream = (IStream) ImageStream, ImageFile;
+        SHCreateStreamOnFileEx (FilePath, 0x1001, 0x80, true, null, out ImageFile);
+        Int32 Data = TotalBlocks > 1024 ? 1024 : 1;
+        Int32 Pointer = TotalBlocks % Data;
+        Int32 SizeBytes = BlockSize * Data;
+        Int32 Buffer = (TotalBlocks - Pointer) / Data;
+        if (Pointer > 0)
+            ResultStream.CopyTo (ImageFile, Pointer * SizeBytes, IntPtr.Zero, IntPtr.Zero);
+        while (Buffer-- > 0)
         {
-            while (TotalBlocks-- > 0)
-            {
-                IStream.Read(Buffer, BlockSize, (IntPtr)Pointer);
-                OpenFile.Write(Buffer, 0, BytesRead);
-            }
-            OpenFile.Flush();
-            OpenFile.Close();
+            ResultStream.CopyTo (ImageFile, SizeBytes, IntPtr.Zero, IntPtr.Zero);
         }
+        ImageFile.Commit (0);
     }
 }
-'@ -CompilerParameters $CompilerParams
-            }
-            $FileSystem = @{ UDF = 4 }; $PlatformId = @{ EFI = 0xEF }
-            ($BootStream = New-Object -ComObject ADODB.Stream -Property @{ Type = 1 } -ErrorAction:$ErrorActionPreference).Open()
-            $BootStream.LoadFromFile((Get-ChildItem -Path "$($ISOMedia.FullName)\efi\Microsoft\boot" -Filter $BootFile | Select-Object -ExpandProperty FullName))
-            ($BootOptions = New-Object -ComObject IMAPI2FS.BootOptions -Property @{ PlatformId = $PlatformId.EFI } -ErrorAction:$ErrorActionPreference).AssignBootImage($BootStream)
-            ($FSImage = New-Object -ComObject IMAPI2FS.MsftFileSystemImage -Property @{ FileSystemsToCreate = $FileSystem.UDF; VolumeName = $InstallInfo.Name; WorkingDirectory = $WorkFolder } -ErrorAction:$ErrorActionPreference).ChooseImageDefaultsForMediaType(13)
+'@
         }
+        $FileSystem = @{ UDF = 4 }; $PlatformId = @{ EFI = 0xEF }
+		($BootStream = New-Object -ComObject ADODB.Stream -Property @{ Type = 1 } -ErrorAction:$ErrorActionPreference).Open()
+        $BootStream.LoadFromFile((Get-ChildItem -Path "$($ISOMedia.FullName)\efi\Microsoft\boot" -Filter $BootFile | Select-Object -ExpandProperty FullName))
+		($BootOptions = New-Object -ComObject IMAPI2FS.BootOptions -Property @{ PlatformId = $PlatformId.EFI; Manufacturer = 'Microsoft'; Emulation = 0 } -ErrorAction:$ErrorActionPreference).AssignBootImage($BootStream)
+        $FSImage = New-Object -ComObject IMAPI2FS.MsftFileSystemImage -Property @{ FileSystemsToCreate = $FileSystem.UDF; UDFRevision = 0x102; FreeMediaBlocks = 0; VolumeName = $InstallInfo.Name; WorkingDirectory = $WorkFolder } -ErrorAction:$ErrorActionPreference
     }
     Process
     {
-        If ($PSVersionTable.PSVersion.Major -lt 6)
-        {
-            ForEach ($Item In Get-ChildItem -Path $ISOMedia.FullName -Force)
-            {
-                If ($Item -isnot [IO.FileInfo] -and $Item -isnot [IO.DirectoryInfo]) { $Item = Get-Item -Path $Item -ErrorAction:$ErrorActionPreference }
-                If ($Item) { $FSImage.Root.AddTree($Item.FullName, $true) }
-            }
-        }
+        $FSImage.Root.AddTree($ISOMedia.FullName, $false)
     }
     End
     {
-        If ($PSVersionTable.PSVersion.Major -lt 6)
+        $FSImage.BootImageOptions = $BootOptions
+        $WriteISO = $FSImage.CreateResultImage()
+        $ISOFile = New-Item -Path $WorkFolder -Name ($($InstallInfo.Edition).Replace(' ', '') + "_$($InstallInfo.Build).iso") -ItemType File -Force -ErrorAction:$ErrorActionPreference
+        If ($ISOFile.Exists)
         {
-            $FSImage.BootImageOptions = $BootOptions
-            $WriteISO = $FSImage.CreateResultImage()
-            $ISOFile = New-Item -Path $WorkFolder -Name ($($InstallInfo.Edition).Replace(' ', '') + "_$($InstallInfo.Build).iso") -ItemType File -Force -ErrorAction:$ErrorActionPreference
-            If ($ISOFile.Exists)
-            {
-                [ISOWriter]::Create($ISOFile.FullName, $WriteISO.ImageStream, $WriteISO.BlockSize, $WriteISO.TotalBlocks)
-                $ISOFile.Refresh()
-                If (($WriteISO.BlockSize * $WriteISO.TotalBlocks) -eq $ISOFile.Length) { $ISOFile.FullName }
-            }
-            While ([Runtime.Interopservices.Marshal]::ReleaseComObject($BootStream) -gt 0) { }
-            While ([Runtime.Interopservices.Marshal]::ReleaseComObject($BootOptions) -gt 0) { }
-            While ([Runtime.Interopservices.Marshal]::ReleaseComObject($FSImage) -gt 0) { }
-            While ([Runtime.Interopservices.Marshal]::ReleaseComObject($WriteISO) -gt 0) { }
-            [GC]::Collect()
-            [GC]::WaitForPendingFinalizers()
+            [ISOWriter]::Create($ISOFile.FullName, [ref]$WriteISO.ImageStream, $WriteISO.BlockSize, $WriteISO.TotalBlocks)
+            $ISOFile.Refresh()
+            If (($WriteISO.BlockSize * $WriteISO.TotalBlocks) -eq $ISOFile.Length) { $ISOFile.FullName }
         }
-        Else
-        {
-            If ($OSCDIMG -and (Test-Path -Path $OSCDIMG))
-            {
-                If (!(Test-Path -Path (GetPath -Path $ISOMedia.FullName -Child 'boot\etfsboot.com'))) { Log "Missing the required etfsboot.com bootfile for ISO creation." -Type Error }
-                $ISOFile = GetPath -Path $WorkFolder -Child ($($InstallInfo.Edition).Replace(' ', '') + "_$($InstallInfo.Build).iso")
-                $BootData = ('2#p0,e,b"{0}"#pEF,e,b"{1}"' -f (Get-ChildItem -Path "$($ISOMedia.FullName)\boot" -Filter etfsboot.com | Select-Object -ExpandProperty FullName), (Get-ChildItem -Path "$($ISOMedia.FullName)\efi\Microsoft\boot" -Filter $BootFile | Select-Object -ExpandProperty FullName))
-                $OSCDIMGArgs = @('-bootdata:{0}', '-u2', '-udfver102', '-l"{1}"', '"{2}"', '"{3}"' -f $BootData, $InstallInfo.Name, $ISOMedia.FullName, $ISOFile)
-                $RET = StartExe $OSCDIMG -Arguments $OSCDIMGArgs
-                If ($RET -eq 0) { $ISOFile }
-            }
-        }
+        While ([Runtime.Interopservices.Marshal]::ReleaseComObject($BootStream) -gt 0) { }
+        While ([Runtime.Interopservices.Marshal]::ReleaseComObject($BootOptions) -gt 0) { }
+        While ([Runtime.Interopservices.Marshal]::ReleaseComObject($FSImage) -gt 0) { }
+        While ([Runtime.Interopservices.Marshal]::ReleaseComObject($WriteISO) -gt 0) { }
+        [GC]::Collect()
+        [GC]::WaitForPendingFinalizers()
     }
 }
