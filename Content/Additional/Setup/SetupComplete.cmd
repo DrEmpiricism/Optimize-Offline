59794184312d0a026e39a76b42d9666a589d84fd
espanafly <69840998+espanafly@users.noreply.github.com>
2022-01-29 14:49:45 +1100

    Script change related to Microsoft design policy

--- Content/Additional/Setup/SetupComplete.cmd
+++ Content/Additional/Setup/SetupComplete.cmd
@@ -4,6 +4,7 @@ REM Answer files should ALWAYS be removed from the 'Windows\Panther' and 'Window
 
 PowerShell.exe -NoProfile -ExecutionPolicy Bypass -File "%WINDIR%\Setup\Scripts\Refresh-Explorer.ps1"
 DEL /F /Q "%WINDIR%\Setup\Scripts\Refresh-Explorer.ps1" >NUL 2>&1
+DEL /F /Q "%WINDIR%\Setup\Scripts\Run_TI_SetupComplete_online.cmd >NUL 2>&1
 DEL /F /Q "%WINDIR%\Panther\unattend.xml" >NUL 2>&1
 DEL /F /Q "%WINDIR%\System32\Sysprep\unattend.xml" >NUL 2>&1
 DEL "%~f0"
