8db256a7e94a928b3ab862fe15eb2abdd9471db9
Genci <genci16@gmail.com>
2022-11-21 00:56:44 +0100

    GUI: fixed error messages not showing in embedded console

--- Start-Optimize.ps1
+++ Start-Optimize.ps1
@@ -115,5 +115,17 @@ Else {
 		Exit
 	}
 }
-
-Optimize-Offline @ConfigParams
+Try{
+	Optimize-Offline @ConfigParams
+} Catch {
+	$formatstring = "{0} : {1}`n{2}`n" +
+                "    + CategoryInfo          : {3}`n" +
+                "    + FullyQualifiedErrorId : {4}`n"
+	$fields = $_.InvocationInfo.MyCommand.Name,
+          $_.ErrorDetails.Message,
+          $_.InvocationInfo.PositionMessage,
+          $_.CategoryInfo.ToString(),
+          $_.FullyQualifiedErrorId
+	Write-Host -Foreground Red ($formatstring -f $fields)
+	Exit
+}
